84e4d9253f9ab86a339278c5d4380b08dd0115e5
Andrii <andriyshopyak@gmail.com>
2023-08-06 12:24:22 +0300

    feat: finished proccesing burger logo algoritm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // Add fuction for burger open
 let burgerIcon = document.querySelector(".nav__burger");
 let mobileBlock = document.querySelector("#mobile__menu");
+let burgerElements = document.querySelectorAll(".nav__burger__item");
 
 burgerIcon.addEventListener("click" , () => {
+    burgerElements[0].classList.toggle("rotateFirst");
+    burgerElements[1].classList.toggle("averrage");
+    burgerElements[2].classList.toggle("rotateLast");
     mobileBlock.classList.toggle("active_mobile");
 });
