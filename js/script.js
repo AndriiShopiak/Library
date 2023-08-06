2b0227d0107d570188a8d8769290120e619c1d03
Andrii <andriyshopyak@gmail.com>
2023-08-06 12:33:35 +0300

    fix: fixed script for burger menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ let burgerIcon = document.querySelector(".nav__burger");
 let mobileBlock = document.querySelector("#mobile__menu");
 let burgerElements = document.querySelectorAll(".nav__burger__item");
 
-burgerIcon.addEventListener("click" , () => {
+function openBueger () {
     burgerElements[0].classList.toggle("rotateFirst");
     burgerElements[1].classList.toggle("averrage");
     burgerElements[2].classList.toggle("rotateLast");
     mobileBlock.classList.toggle("active_mobile");
-});
+}
+
+burgerIcon.addEventListener("click" , openBueger);
