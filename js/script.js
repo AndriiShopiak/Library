cd18cebc92b5bcbc910ff454724acacffdd40346
Andrii <andriyshopyak@gmail.com>
2023-08-06 12:48:57 +0300

    feat: add close menu function for links

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 let burgerIcon = document.querySelector(".nav__burger");
 let mobileBlock = document.querySelector("#mobile__menu");
 let burgerElements = document.querySelectorAll(".nav__burger__item");
+let welcomeSec = document.querySelector("#welcome");
+let itemsMobile = document.querySelectorAll("#item__mobile_menu");
 
 function openBueger () {
     burgerElements[0].classList.toggle("rotateFirst");
@@ -11,3 +13,7 @@ function openBueger () {
 }
 
 burgerIcon.addEventListener("click" , openBueger);
+welcomeSec.addEventListener("click" , openBueger);
+itemsMobile.forEach ((e) => {
+    e.addEventListener("click", openBueger);   
+});
